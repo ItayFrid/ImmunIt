c5c3567ac12c479cfacc584bc428c0fcac746023
Itay Fridman <fitay123@gmail.com>
2019-03-20 17:28:43 +0200

    Add ViewModel

---
+++ ImmunIt/ViewModels/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ImmunIt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImmunIt.ViewModels
+{
+    public class ViewModel
+    {
+        public ImmunCard immunCard { get; set; }
+        public List<ImmunCard> immunCards { get; set; }
+        public Medic medic { get; set; }
+        public List<Medic> medics { get; set; }
+        public User user { get; set; }
+        public List<User> users { get; set; }
+    }
+}
