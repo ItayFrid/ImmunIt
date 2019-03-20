bcfd3343156a0f4ae672e3745b31ec2d105c8f8f
Amir Aizin <amiraizin20@gmail.com>
2019-03-20 17:16:06 +0200

    Added Medic class.

--- ImmunIt/Models/Medic.cs
+++ ImmunIt/Models/Medic.cs
@@ -7,5 +7,20 @@ namespace ImmunIt.Models
 {
     public class Medic
     {
+        [Key]
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "Licence Number must be  5 characters")]
+        public string LicenceNumber { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be 5 to 50 characters")]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string role { get; set; }
+
+
     }
 }
