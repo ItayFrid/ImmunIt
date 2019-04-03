12f441ef438f1c21b8686f9022befaf87a0757f6
Itay Fridman <fitay123@gmail.com>
2019-04-03 15:16:55 +0300

    Add Controllers

---
+++ ImmunIt/Controllers/MedicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImmunIt.Controllers
+{
+    public class MedicController : Controller
+    {
+        // GET: Medic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
