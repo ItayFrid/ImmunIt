64b215651d0bfc6f4951c50f39690605db47c3fa
Amir Aizin <amiraizin20@gmail.com>
2019-04-03 16:26:50 +0300

    Working on Manager Login

--- ImmunIt/Controllers/ManagerController.cs
+++ ImmunIt/Controllers/ManagerController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ImmunIt.DAL;
+using ImmunIt.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ImmunIt.Controllers
 {
@@ -11,11 +14,48 @@ namespace ImmunIt.Controllers
         // GET: Manager
         public ActionResult Index()
         {
+
             return View();
         }
         public ActionResult Login()
         {
-            return View();
+            Manager mng = new Manager();
+            return View(mng);
+        }
+
+        public ActionResult ManagerLogin(Manager mng)
+        {
+
+            DataLayer dal = new DataLayer();
+            //Encryption enc = new Encryption();
+            List<Manager> ManagerToCheck = (from x in dal.Managers
+                                         where x.UserName == mng.UserName
+                                         select x).ToList<Manager>();       //Attempting to get user information from database
+            if (ManagerToCheck.Count != 0)     //In case username was found
+            {
+                // if (enc.ValidatePassword(user.password, userToCheck[0].password))   //Correct password
+                //{
+                var authTicket = new FormsAuthenticationTicket(
+                    1,                                  // version
+                    mng.UserName,                      // user name
+                    DateTime.Now,                       // created
+                    DateTime.Now.AddMinutes(20),        // expires
+                    true,       //keep me connected
+                    ManagerToCheck[0].role                       // store roles
+                    );
+
+                string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+
+                var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                Response.Cookies.Add(authCookie);
+                return RedirectToAction("Index","Manager");
+                //}
+
+            }
+            else
+                ViewBag.UserLoginMessage = "Incorrect Username/password";
+            return View("ManagerLogin", mng);
+
         }
         public ActionResult AddMedic()
         {
